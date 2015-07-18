53a83d4d4bb5b1b141f174493e3bf7670fd5b083
Ken DeLong <kenwdelong@yahoo.com>
2015-07-18 13:33:11 -0700

    test metrics using it with Grafana

--- src/test/java/com/kendelong/util/monitoring/graphite/GraphiteClientTest.java
+++ src/test/java/com/kendelong/util/monitoring/graphite/GraphiteClientTest.java
@@ -14,7 +14,7 @@ public class GraphiteClientTest
 	{
 		GraphiteClient gc = new GraphiteClient();
 		gc.setAppName("gc");
-		gc.setStatsdHost("127.0.0.1");
+		gc.setStatsdHost("192.168.59.103");
 		gc.setStatsdPort(8125);
 		gc.setServerEnv("test");
 		gc.init();
@@ -24,7 +24,8 @@ public class GraphiteClientTest
 		Random random = new Random();
 		for(int i = 0; i < 10000; i++)
 		{
-			gc.increment("theTesterIncrement");
+			gc.increment("a.foo");
+			gc.increment("b.foo");
 			gc.count("theTesterCount", 1);
 			gc.time("theTesterTime", (long) (300*(1 + random.nextDouble() - 0.5)));
 			Thread.sleep(100);
@@ -33,5 +34,23 @@ public class GraphiteClientTest
 		
 		System.out.println("Done");
 	}
-
+	
+	/*
+	 * gc.increment("a.foo");
+	 * gc.increment("b.foo");
+	 * 
+	 * a.foo.count = 100
+	 * Grafana buckets are 10 seconds wide!
+	 * 
+	 * a.foo.rate = 10
+	 * sumSeries(*.foo.rate) = 20
+	 * 
+	 * $ docker run -p 8000:80/tcp -p 8125:8125/udp -p 8126:8126/tcp --name grafana cooniur/grafana_graphite
+	 * http://192.168.59.103:8000/#/dashboard/db/welcome?panelId=3&fullscreen&edit
+	 * That IP is the IP of the boot2docker VM!  
+	 * Shown when
+	 *   - you start boot2docker
+	 * 
+	 * 
+	 */
 }
