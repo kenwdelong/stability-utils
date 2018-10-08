119ba26f63d79be066659735b6686efa3432c356
Ken DeLong <kenwdelong@gmail.com>
2018-10-08 12:17:56 -0700

    typo

--- src/main/java/com/kendelong/util/monitoring/webservice/WebServiceEndpoint.java
+++ src/main/java/com/kendelong/util/monitoring/webservice/WebServiceEndpoint.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 import com.kendelong.util.performance.MonitorPerformance;
 
 /**
- * Drop this annotation, along with @MonitorPerformance, on a class that serves as a web service endpoint (typically a contrller).  This will cause the 
+ * Drop this annotation, along with @MonitorPerformance, on a class that serves as a web service endpoint (typically a controller).  This will cause the 
  * bean to be registered in a JMX domain that includes "webservice.endpoint", and for any data sent to Graphite (if it's configured)
  * to also be under a similar namespace.  In this way, you have an easy way to keep track of all your webservices that you've implemented
  * automatically - they won't be "forgotten".
